579f2baef3dfcd520aa3c9641e6d3ad01d686a37
Vitor Bacelar de Paula Augusto <vitor.bacelar@protonmail.com>
2023-11-25 12:46:32 -0300

    add sign in with goggle

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,38 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const Login());
 }
 
 class Login extends StatelessWidget {
   const Login({Key? key}) : super(key: key);
+
+  Future<UserCredential> signInWithGoogle() async {
+    // Trigger the authentication flow
+    final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
+
+    // Obtain the auth details from the request
+    final GoogleSignInAuthentication? googleAuth =
+        await googleUser?.authentication;
+
+    // Create a new credential
+    final credential = GoogleAuthProvider.credential(
+      accessToken: googleAuth?.accessToken,
+      idToken: googleAuth?.idToken,
+    );
+
+    // Once signed in, return the UserCredential
+    print(credential.accessToken);
+    return await FirebaseAuth.instance.signInWithCredential(credential);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -21,7 +48,7 @@ class Login extends StatelessWidget {
               const SizedBox(height: 20), // Add space of 20 pixels
               ElevatedButton(
                 onPressed: () {
-                  print("fon");
+                  signInWithGoogle();
                 },
                 child: const Text(
                   'Login',
